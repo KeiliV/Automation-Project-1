e59ce47e48bfc0e4acc5bf66107644915e30d275
Keili <keili.vutt@gmail.com>
2023-12-26 22:30:09 +0200

    Proj5: time tracking add estimation and validate done

--- Project_2/cypress/e2e/issue-time-tracking.cy.js
+++ Project_2/cypress/e2e/issue-time-tracking.cy.js
@@ -1,5 +1,5 @@
 //CREATE NEW ISSUE
-it("Should create an issue successfully", () => {
+it.skip("Should create an issue successfully", () => {
   cy.visit("/");
   cy.url()
     .should("eq", `${Cypress.env("baseUrl")}project/board`)
@@ -32,22 +32,36 @@ describe("Time Tracking Functionality", () => {
       .should("eq", `${Cypress.env("baseUrl")}project`)
       .then((url) => {
         cy.visit(url + "/board");
-        cy.contains("Time estimation test ticket").click();
+        cy.contains("This is an issue of type: Task.").click();
       });
   });
 
   it("Should add, edit and remove time estimation successfully", () => {
+    //Remove estimated time for testing purposes
+    cy.get('input[placeholder="Number"]').eq(0).clear();
+    //Remove time already logged for testing purposes
+    cy.get('[data-testid="icon:stopwatch"]').next().click();
+    cy.get('[data-testid="modal:tracking"]').should("be.visible");
+    cy.get('input[placeholder="Number"]').eq(1).click().clear();
+    cy.contains("button", "Done").click();
+
     //Check that time tracker has no spent time added (“No Time Logged” label is visible)
     cy.get('[data-testid="icon:stopwatch"]').next().contains("No time logged");
 
     //User adds value 10 to “Original estimate (hours)” field
+    cy.get('input[placeholder="Number"]').eq(0).type(10).blur(); //.wait(1000);
+
+    //VALIDATION-1
+    //Entered value in hours is visible in the time tracking section
+    cy.get('[data-testid="icon:stopwatch"]').next().contains("10h estimated");
 
     //User closes issue detail page
-
+    cy.get('[data-testid="icon:close"]').first().click();
     //User reopens the same issue to check that original estimation is saved
+    cy.contains("This is an issue of type: Task.").click();
 
-    //VALIDATIONS
-    //Entered value in hours is visible in the time tracking section
+    //VALIDATION-2
     //After reopening issue detail view estimation value is still visible
+    cy.get('input[placeholder="Number"]').eq(0).should("have.value", "10");
   });
 });
