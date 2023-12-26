d7a437e64ad0994b587b9c399f8d02a028837207
Keili <keili.vutt@gmail.com>
2023-12-26 20:34:58 +0200

    Proj5: time tracking adding first test

--- Project_2/cypress/e2e/issue-time-tracking.cy.js
+++ Project_2/cypress/e2e/issue-time-tracking.cy.js
@@ -7,7 +7,9 @@ it("Should create an issue successfully", () => {
       cy.visit(url + "/board?modal-issue-create=true");
     });
   cy.get('[data-testid="modal:issue-create"]').within(() => {
-    cy.get('input[name="title"]').type("Time estimation test ticket");
+    cy.get('input[name="title"]')
+      .wait(1000)
+      .type("Time estimation test ticket");
     cy.get('input[name="title"]').should(
       "have.value",
       "Time estimation test ticket"
@@ -16,8 +18,36 @@ it("Should create an issue successfully", () => {
   });
 
   cy.get('[data-testid="modal:issue-create"]').should("not.exist");
+  cy.reload();
 
   cy.get('[data-testid="board-list:backlog"]').contains(
     "Time estimation test ticket"
   );
+});
+
+describe("Time Tracking Functionality", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.url()
+      .should("eq", `${Cypress.env("baseUrl")}project`)
+      .then((url) => {
+        cy.visit(url + "/board");
+        cy.contains("Time estimation test ticket").click();
+      });
+  });
+
+  it("Should add, edit and remove time estimation successfully", () => {
+    //Check that time tracker has no spent time added (“No Time Logged” label is visible)
+    cy.get('[data-testid="icon:stopwatch"]').next().contains("No time logged");
+
+    //User adds value 10 to “Original estimate (hours)” field
+
+    //User closes issue detail page
+
+    //User reopens the same issue to check that original estimation is saved
+
+    //VALIDATIONS
+    //Entered value in hours is visible in the time tracking section
+    //After reopening issue detail view estimation value is still visible
+  });
 });
