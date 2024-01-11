b3e2e81b5a19b3c6ede569974b52304e9da4daa6
Keili <keili.vutt@gmail.com>
2024-01-11 17:22:13 +0200

    Proj5: Bonus assignment Task 1 finished

--- Project_2/cypress/e2e/issue-details-edit.cy.js
+++ Project_2/cypress/e2e/issue-details-edit.cy.js
@@ -71,46 +71,80 @@ describe("Issue details editing", () => {
   const getIssueDetailsModal = () =>
     cy.get('[data-testid="modal:issue-details"]');
 
-  it.only("Should loop through elements, save text values to an array, and assert array length", () => {
+  it("Should loop through elements, save text values to an array, and assert array length", () => {
     //Predefine variable for expected number of elements in the priority dropdown, for example, “const expectedLength
-    const expectedLength = 4;
+    const expectedLength = 5;
     //Predefine an empty array variable. Decide which definition is needed: const or let.
     let prioritiesArray = [];
+
+    const getSelectPriorityDropdownControl = () =>
+      cy.get('[data-testid="select:priority"]');
     // CYPRESS ALIASES
-    // cy.wrap(prioritiesArray).as("prioritiesArray");
+    //cy.wrap(prioritiesArray).as("prioritiesArray");
 
     getIssueDetailsModal().within(() => {
-      // grab the current selected option
-      const selectPriorityDropdownControl = cy.get(
-        '[data-testid="select:priority"]'
-      );
+      // grab the current selected option and push it to the predefined array
 
-      selectPriorityDropdownControl.should("have.length", 1);
+      getSelectPriorityDropdownControl().should("have.length", 1);
 
-      selectPriorityDropdownControl.invoke("text").then((text) => {
-        prioritiesArray.push(text);
-      });
-
+      getSelectPriorityDropdownControl()
+        .invoke("text")
+        .then((text) => {
+          prioritiesArray.push(text);
+        });
+      //IF I want to validate that the intial value was pushed into the array:
       cy.then(() => expect(prioritiesArray).to.eql(["High"]));
 
-      // USE CYPRESS ALIAS INSTEAD OF THEN
-      // cy.get("@prioritiesArray").should("eql", ["High"]);
+      // ALTERNATIVELY USE CYPRESS ALIAS INSTEAD OF .THEN, also must define alias-> see .wrap above
+      //cy.get("@prioritiesArray").should("eql", ["High"]);
 
-      // begin example of how anonymous funtions (also called lambdas) can be used instead of named functions
-      // this
-      //   cy.wrap(prioritiesArray).then( (ary) => expect(ary).to.eql(["High"]) );
-      // is exactly the same as this
-      //   function assertTheExpectation (ary) {
-      //     return expect(ary).to.eql(["High"])
-      //   }
-      //   cy.wrap(prioritiesArray).then( assertTheExpectation );
-      // end
+      //Loop through the elements: each time, invoke the text value from the current element and save it into your predefined array.
+      //Print out the added value and length of the array during each iteration using the cy.log(...…) command.
+      //cy.get('[data-testid="select:priority"]').click();
+      getSelectPriorityDropdownControl().click();
+      cy.get('[data-testid^="select-option:"]').each(($element) => {
+        const text = $element.text();
+        prioritiesArray.push(text);
+        cy.log(text);
+        cy.log("length", prioritiesArray.length);
+        //LONGER WAY TO DO IT, but this has a callback inside of a callback. ALSO I was refetching &element when I alreday had fetched it
+
+        // cy.get($element)
+        //   .invoke("text")
+        //   .then((text) => {
+        //     prioritiesArray.push(text);
+        //     cy.log(text);
+        //     cy.log("length", prioritiesArray);
+        //   });
+      });
+
+      //Assert that the array created has the same length as your predefined number if everything is done correctly.
+      //Expected result: You have a test that validates values in issue priorities. The finished array must have five elements: [“Lowest“, “Low“, “Medium”, “High“, “Highest”]
+      cy.then(() => {
+        expect(prioritiesArray).to.include.members([
+          //Look up Chai assertions when using expect
+          "High",
+          "Highest",
+          "Medium",
+          "Low",
+          "Lowest",
+        ]);
+
+        expect(prioritiesArray).to.have.lengthOf(expectedLength);
+      });
     });
   });
+
+  it.only("Assignment 3, Task 2", () => {});
 });
 
-//Loop through the elements: each time, invoke the text value from the current element and save it into your predefined array.
-//Print out the added value and length of the array during each iteration using the cy.log(...…) command.
-//Assert that the array created has the same length as your predefined number if everything is done correctly.
-
-//Expected result: You have a test that validates values in issue priorities. The finished array must have five elements: [“Lowest“, “Low“, “Medium”, “High“, “Highest”]
+//FOR LEARNING
+// begin example of how anonymous funtions (also called lambdas) can be used instead of named functions
+// this
+//   cy.wrap(prioritiesArray).then( (ary) => expect(ary).to.eql(["High"]) );
+// is exactly the same as this
+//   function assertTheExpectation (ary) {
+//     return expect(ary).to.eql(["High"])
+//   }
+//   cy.wrap(prioritiesArray).then( assertTheExpectation );
+// end
